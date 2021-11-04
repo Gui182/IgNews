81ca687ee8c0d3bb7a4f64c4e218f1ccdf3ea592
GuiLaTT <guilherme.oliveira182@yahoo.com.br>
2021-11-04 13:40:35 -0300

    termino do modulo jamstack

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/dist/client/router'
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
@@ -20,10 +21,16 @@ type SessionId = {
 
 export function SubscribeButton( { priceId }: SubscribeButtonProps ) {
     const [session] = useSession()
+    const router = useRouter()
     
     async function handleSubscribe() {
         if(!session){
             signIn('github')
+            return
+        }
+
+        if(session.activeSubscription) {
+            router.push('/posts')
             return
         }
 
